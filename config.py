60e400a98f0609fde0c74ded64c633129b1ccc5e
kevin-kili <kilikevin435@gmail.com>
2020-04-25 20:37:25 +0300

    added content in manage.py

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    TOP_NEWS_URL = "https://newsapi.org/v2/top-headlines?language=en&apiKey={}"
+    TOP_NEWS_BY_SOURCE_URL = "https://newsapi.org/v2/top-headlines?sources={}&apiKey={}"
+    SEARCH_NEWS_URL = "https://newsapi.org/v2/top-headlines?q={}&apiKey={}"
+    CATEGORY_NEWS_URL = "https://newsapi.org/v2/top-headlines?category={}&apiKey={}"
+    NEWS_SOURCES_URL = "https://newsapi.org/v2/sources?apiKey={}"
+
+    # API key is stored as environmental variable
+    NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig
+}
